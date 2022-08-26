ab178be79456a7ed7f85fd67ab490ee6361a2580
Guru Bhargava <88764015+Guru-Bhargava@users.noreply.github.com>
2022-08-26 08:41:46 +0530

    Create Print name N times using recursion.cpp

---
+++ Step 1/Step 1.4/Print name N times using recursion.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define ll long long
+ll cnt=0;
+void solve()
+{
+  if(cnt==18) return;
+  cout<<"Virat Kohli"<<endl;
+  cnt++;
+  solve();
+}
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  // ll T;
+  ll T = 1;
+  // cin >> T;
+  while (T--) {
+    solve();
+  }
+}
