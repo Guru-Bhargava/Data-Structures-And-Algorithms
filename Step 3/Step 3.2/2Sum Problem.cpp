b4fcdc86020ff55d057d537c372d8b98a2df1fb9
Guru Bhargava <88764015+Guru-Bhargava@users.noreply.github.com>
2022-08-27 10:30:16 +0530

    Update 2Sum Problem.cpp

--- Step 3/Step 3.2/2Sum Problem.cpp
+++ Step 3/Step 3.2/2Sum Problem.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>>v;
+        for(int i=0;i<nums.size();++i)
+        {
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        int l=0,r=nums.size()-1;
+        while(l<=r)
+        {
+            if(v[l].first+v[r].first==target) return{v[l].second,v[r].second};
+            else if(v[l].first+v[r].first>target) r--;
+            else l++;
+        }
+        return{0,0};
+        }
+};
 
