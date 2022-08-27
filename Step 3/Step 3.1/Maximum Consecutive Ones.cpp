dff843ef535001acc07c122ff19658c1ff24b314
Guru Bhargava <88764015+Guru-Bhargava@users.noreply.github.com>
2022-08-27 13:24:52 +0530

    Create 	Maximum Consecutive Ones.cpp

---
+++ Step 3/Step 3.1/Maximum Consecutive Ones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt=0,maxi=-1;
+        for(int i=0;i<nums.size();++i)
+        {
+            if(nums[i]==1) cnt++;
+            else
+            {
+                maxi=max(maxi,cnt);
+                cnt=0;
+            }
+            maxi=max(maxi,cnt);
+        }
+        return(maxi);
+    }
+};
