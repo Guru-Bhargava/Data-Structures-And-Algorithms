d67d4a5f36829d1f20fae0b76d8a7bde9595123b
Guru Bhargava <88764015+Guru-Bhargava@users.noreply.github.com>
2022-09-21 13:27:50 +0530

    Update House Robber (DP 6).cpp

--- Step 16/Step 16.2/House Robber (DP 6).cpp
+++ Step 16/Step 16.2/House Robber (DP 6).cpp
@@ -1,19 +1,37 @@
-class Solution
-{
-    public:
-    //Function to find the maximum money the thief can get.
-    int f(int ind, int v[] ,vector<int> &dp)
-    {
-    	if(ind==0) return(v[ind]);
-    	if(dp[ind]!=-1) return(dp[ind]);
-    	int pick=v[ind]+f(ind-2,v,dp);
-    	int notpick=0+f(ind-1,v,dp);
-    	return(dp[ind]=max(pick,notpick));
-    }
+#include<bits/stdc++.h>
+using namespace std;
 
-    int FindMaxSum(int arr[], int n)
-    {
-        vector<int>dp(n+1,-1);
-	    return (f(n-1,arr,dp));
-    }
-};
+int f(int ind,vector<int>&v,vector<int>&dp)
+{
+	if(ind==0) return(v[0]);
+	if(dp[ind]!=-1) return(dp[ind]);
+	if(ind==2) return(dp[2]=max(v[1],v[0]));
+	int pick=f(ind-2,v,dp)+v[ind];
+	int notpick=f(ind-1,v,dp)+0;
+	return(dp[ind]=max(pick,notpick));
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	vector<int>v(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>v[i];
+	}
+	vector<int>dp1(n,-1);
+	vector<int>dp2(n,-1);
+	vector<int>v1,v2;
+	for(int i=0;i<n-1;++i)
+	{
+		v1.push_back(v[i]);
+	}
+	for(int i=1;i<n;++i)
+	{
+		v2.push_back(v[i]);
+	}
+	int a=f(n-1,v1,dp1);
+	int b=f(n-1,v2,dp2);
+	cout<<max(a,b);
+}
